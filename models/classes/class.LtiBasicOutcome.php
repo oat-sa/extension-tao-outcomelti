42c13af8712573c8696f70c5fbdaeda4896b3817
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-08-27 12:06:17 +0000

    code cleaning
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoLtiBasicOutcome/trunk@15591 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/classes/class.LtiBasicOutcome.php
+++ models/classes/class.LtiBasicOutcome.php
@@ -1,36 +1,28 @@
 <?php
 
-//require_once("taoLtiBasicOutcome/includes/ims-blti/OAuthBody.php");
-
-//The ResultStorage does not provide a good interface for the LTI submission case, may require some more abstract interface
-
-// LtiBasicOutcome relies on a speciifc property added to the result server that is the lti consumer which to send the results to (secret is retrieved)
-// LtiBasicOutcome relies on a custom option given by the service submitting results taht is the url
+/**
+ * Implements tao results storage with respect to LTI 1.1.1 specs acting as a Tool provider calling back the consumer outcome service
+ *
+ */
 
 class taoLtiBasicOutcome_models_classes_LtiBasicOutcome
     extends tao_models_classes_GenerisService
     implements taoResultServer_models_classes_ResultStorage {
 
-    private $ltiConsumer;//the kb resource modelling the LTI consumer
+    //private $ltiConsumer;//the kb resource modelling the LTI consumer
     /**
     * @param string deliveryResultIdentifier if no such deliveryResult with this identifier exists a new one gets created
     */
-
     public function __construct(){
 		parent::__construct();
         common_ext_ExtensionsManager::singleton()->getExtensionById("taoLtiBasicOutcome");
-        //$this->consumer
-       
     }
-
     /**
-     *
      * @param type $deliveryResultIdentifier lis_result_sourcedid
      * @param type $test ignored
      * @param taoResultServer_models_classes_Variable $testVariable
      * @param type $callIdTest ignored
      */
-
     public function storeTestVariable($deliveryResultIdentifier, $test, taoResultServer_models_classes_Variable $testVariable, $callIdTest){
        
         if (get_class($testVariable)=="taoResultServer_models_classes_OutcomeVariable") {
@@ -39,7 +31,6 @@ class taoLtiBasicOutcome_models_classes_LtiBasicOutcome
                 . "Result Identifier (".$deliveryResultIdentifier.")"
                 . "Service URL (".$this->serviceUrl.")"
                 );
-            
             $variableIdentifier = $testVariable->getIdentifier();
             //if in_array($variableIdentifier, array("numberCorrect", "numberSelected", "ratio"))
             //
@@ -55,15 +46,12 @@ class taoLtiBasicOutcome_models_classes_LtiBasicOutcome
             //Building POX raw http message
             $unSignedOutComeRequest = new common_http_Request($this->serviceUrl, 'POST', array());
             $unSignedOutComeRequest->setBody($message);
-           
             $signingService = new tao_models_classes_oauth_Service();
             $signedRequest = $signingService->sign($unSignedOutComeRequest, $credentials, true );
             
              //Hack for moodle comaptibility, the header is ignored for the signature computation
             $signedRequest->setHeader("Content-Type", "application/xml");
 
-           // $signedRequest->setBody($message);
-            //common_Logger::i("Signed Request :\n\n". serialize($signedRequest)."\n\n");
             $response = $signedRequest->send();
             common_Logger::i("Response received from the outcome service with http code ".serialize($response)."" );
             if ($response->httpCode != "200") {
@@ -71,15 +59,12 @@ class taoLtiBasicOutcome_models_classes_LtiBasicOutcome
                 throw new common_Exception("An HTTP level proble occured when sending the outcome to the service url");
             }
             
-            //var_dump($response);
-            
         }
        
     }
-
     /*
-         * retrieve specific parameters from the resultserver to configure the storage
-         */
+    * retrieve specific parameters from the resultserver to configure the storage
+    */
     /*sic*/
     public function configure(core_kernel_classes_Resource $resultserver, $callOptions = array()) {
         /**
@@ -102,24 +87,21 @@ class taoLtiBasicOutcome_models_classes_LtiBasicOutcome
         
     }
      /**
-     * In the case of An LtiBasic OutcomeSubmission, spawnResult is not supported
+     * In the case of An LtiBasic OutcomeSubmission, spawnResult has no effect
      */
     public function spawnResult(){
        //
     }
     public function storeRelatedTestTaker($deliveryResultIdentifier, $testTakerIdentifier) {
-
     }
 
     public function storeRelatedDelivery($deliveryResultIdentifier, $deliveryIdentifier) {
-
     }
 
     public function storeItemVariable($deliveryResultIdentifier, $test, $item, taoResultServer_models_classes_Variable $itemVariable, $callIdItem){
             //for testing purpose
             common_Logger::i("Item Variable Submission: ".$itemVariable->getIdentifier() );
             $this->storeTestVariable($deliveryResultIdentifier, $test, $itemVariable, $callIdItem);
-
     }
 
 }

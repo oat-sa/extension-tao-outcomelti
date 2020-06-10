d819634defd0993dcb497688f938854379253a92
gitromba <mihaly.hajdu@taotesting.com>
2020-06-10 12:02:01 +0200

    chore: bump manifest version

--- manifest.php
+++ manifest.php
@@ -13,7 +13,7 @@ return [
   'label' => 'Result storage for LTI',
     'description' => 'Implements the LTI basic outcome engine for LTI Result Server',
   'license' => 'GPL-2.0',
-  'version' => '3.2.1',
+  'version' => '3.3.0',
     'author' => 'Open Assessment Technologies',
     'requires' => [
         'taoResultServer' => '>=4.2.0',
